c0f2fd80dbab16a14f97d40b58f37745805e08c2
krushme <kelsieberninger@gmail.com>
2021-08-17 19:31:59 -0500

    studio complete

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -5,7 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
 })
+
 export class CrewComponent implements OnInit {
+  memberBeingEdited: object = null;
 
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
@@ -16,6 +18,24 @@ export class CrewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
   }
 
 }
